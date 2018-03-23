9a92b7c2fdf27ac702d8578d86ed91d6bbe8283d
makitodev <makitodev@outlook.com>
2018-03-23 12:44:43 -0500

    Creación de clase workers que son los o subprocesos (childprocess) que se levantarán de forma paralela.

---
+++ Process/MultiProcess/workers.js
@@ -0,0 +1,13 @@
+//Listener de mensajes
+process.on('message', (d)=> { 
+    // ---> array [1...x] suma todos los elementos del arreglo
+    var result = d.arreglo.reduce((a,x)=> a+x); 
+    process.send({result: result});
+    //process.exit([code])
+    //Ends the process with the specified code. 
+    //If omitted, exit uses the 'success' code 0.
+    //To exit with a 'failure' code:
+    //process.exit(1);
+    //The shell that executed node should see the exit code as 1.
+    process.exit(0);
+});
